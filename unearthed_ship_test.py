9affca7ff8b88c7a0209348e3385fd77ecdc3d9b
Angela Xie <jaguarheart0418@gmail.com>
2025-09-13 14:57:34 -0400

    code as of 9-13-2025

--- unearthed_ship_test.py
+++ unearthed_ship_test.py
@@ -5,17 +5,18 @@ from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
 hub = PrimeHub()
-right = Motor(Port.A, Direction.CLOCKWISE)
-left = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+right = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+left = Motor(Port.B, Direction.CLOCKWISE)
 right_side = Motor(Port.E, Direction.CLOCKWISE)
 left_side = Motor(Port.F, Direction.COUNTERCLOCKWISE)
 drive_base = DriveBase(right, left, 62.4, 110)
 
 drive_base.use_gyro(True)
 drive_base.settings(straight_speed = 300)
-drive_base.straight(400)
+drive_base.straight(370)
 drive_base.settings(straight_speed = 700)
-drive_base.straight(197.33)
+drive_base.straight(250)
 drive_base.settings(straight_speed = 1000)
 drive_base.straight(-10)
 drive_base.straight(-340)
+drive_base.curve(radius = 250, angle = -35)
