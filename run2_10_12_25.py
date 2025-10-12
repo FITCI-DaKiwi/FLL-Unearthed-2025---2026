58316feb5dc4ec2a5c90af42800a774b8e80f9de
Alexloveslego <bobolovesalex@gmail.com>
2025-10-12 12:02:57 -0400

    101225 incomplete run 2

---
+++ run2_10_12_25.py
@@ -0,0 +1,42 @@
+from pybricks.hubs import PrimeHub
+from pybricks.parameters import Direction, Port
+from pybricks.pupdevices import Motor
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+# Set up all devices.
+prime_hub = PrimeHub()
+left = Motor(Port.A, Direction.CLOCKWISE)
+right = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+leftattach = Motor(Port.E, Direction.COUNTERCLOCKWISE)
+rightattach = Motor(Port.F, Direction.CLOCKWISE)
+drive_base = DriveBase(right, left, 81.6, 101.7)
+
+drive_base.settings(400)
+# MAKE ARM UNDER HORIZONATL OR ESLE
+drive_base.use_gyro(True)
+heading=0
+print('tHIS iS dAIlY NeWs pREsEnTInG aLeXS BrAiNCeLl LOsS pEr SeCoNd, WhICh iS ', prime_hub.battery.voltage())
+print('aLExs iQ LoSs iS  ', prime_hub.battery.current())
+rightattach.run_angle(500,-95)
+drive_base.straight(680)
+#rightattach.run_angle(100,-400)
+#now beginning to wack wack wack the silo for free food
+rightattach.run_angle(500,105)
+rightattach.run_angle(500,-105)
+rightattach.run_angle(500,105)
+rightattach.run_angle(500,-105)
+rightattach.run_angle(500,105)
+rightattach.run_angle(500,-105)
+drive_base.straight(205)
+drive_base.turn(-80)
+drive_base.straight(625)
+drive_base.settings(400,200,60,120)
+drive_base.turn(50)
+drive_base.settings(400)
+drive_base.straight(100)
+# drive_base.straight(-20)
+# drive_base.turn()
+# drive_base.settings(500)
+# drive_base.straight(700)
+# drive_base.straight(-500)
