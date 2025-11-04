b6765086c616759bcfeb0a1aefe09aac6048db6d
lisaisapizza <ylisadai@gmail.com>
2025-11-03 20:16:35 -0500

    FitciAllianceTechShowSavageShipwreck
    cool

---
+++ FitciAllianceTechShowSavageShipwreck.py
@@ -0,0 +1,15 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+left = Motor(Port.A, Direction.CLOCKWISE)
+right = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+right_side = Motor(Port.E, Direction.CLOCKWISE)
+left_side = Motor(Port.F, Direction.COUNTERCLOCKWISE)
+drive_base = DriveBase(left, right, 62.4, 110)
+
+drive_base.straight(270)
+drive_base.straight(-270)
