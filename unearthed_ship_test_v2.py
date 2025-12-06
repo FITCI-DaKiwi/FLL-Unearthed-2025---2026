6bebeacbfaa5073610269b227727274320bad30e
Angela Xie <jaguarheart0418@gmail.com>
2025-12-06 18:01:33 -0500

    as of dec. 6th, 2025

---
+++ unearthed_ship_test_v2.py
@@ -0,0 +1,43 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+from dakiwi_lib import move_straight, move_straight_v1, turn_exact
+
+hub = PrimeHub()
+left = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+right = Motor(Port.B, Direction.CLOCKWISE)
+right_side = Motor(Port.E, Direction.CLOCKWISE)
+left_side = Motor(Port.F, Direction.COUNTERCLOCKWISE)
+drive_base = DriveBase(left, right, 62.4, 113)
+# heading=0
+
+# the initial position is:
+# back: 3rd thin line 
+# side: dark black line
+# dead fossilized chicken initial position is pointing up
+# drive_base.use_gyro(True)
+
+heading = hub.imu.heading()
+print('before start, the heading is:', heading)
+
+
+move_straight_v1(hub, drive_base, -320, 150)
+print('tHIS iS dAIlY NeWs pREsEnTInG aLeXS BrAiNCeLl LOsS pEr SeCoNd, WhICh iS ', hub.battery.voltage())
+# # drive_base.use_gyro(False)
+move_straight_v1(hub, drive_base, -150, 100)
+# put flag down
+left_side.run_angle(300, 100)
+move_straight_v1(hub, drive_base, 10, 500)
+move_straight_v1(hub, drive_base, 350, 500)
+drive_base.curve(-337,51)
+# turn_exact(hub, drive_base, right, left, 90)
+move_straight_v1(hub, drive_base, -367, 333)
+turn_exact(hub, drive_base, right, left, 80)
+right_side.run_angle(500, 120)
+move_straight_v1(hub, drive_base, 125, 257)
+right_side.run_angle(1000, -90)
+move_straight_v1(hub, drive_base, -100, 15) 
+
+
