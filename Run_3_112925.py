1b92e1bc2f62f3a6dd03240026e73e55a4d1b18d
TylerZheng256 <tylerzhengsd@gmail.com>
2025-12-06 17:47:09 -0500

    new code made on december 6
    we worked on accuracy, efficiency and other things

--- Run_3_112925.py
+++ Run_3_112925.py
@@ -3,7 +3,7 @@ from pybricks.parameters import Direction, Port
 from pybricks.pupdevices import Motor
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
-from dakiwi_lib import turn_exact,move_straight
+
 # Set up all devices.
 prime_hub = PrimeHub()
 left = Motor(Port.A, Direction.CLOCKWISE)
@@ -24,6 +24,27 @@ drive_base.straight(-600)
 drive_base.straight(185)
 leftattach.run_angle(670, 90)
 drive_base.turn(45)
-drive_base.straight(-600)
-drive_base.turn(-90)
-drive_base.straight(-250)
+drive_base.straight(-580)
+drive_base.settings(175)
+drive_base.turn(-95)
+drive_base.settings(300)
+drive_base.straight(-100)
+drive_base.settings(500)
+wait(410)
+drive_base.settings(600)
+drive_base.straight(-300)
+drive_base.turn(-35)
+drive_base.straight(200)
+drive_base.turn(-20)
+drive_base.straight(300)
+drive_base.turn(-20)
+drive_base.straight(-700)
+
+
+
+
+
+
+
+
+
