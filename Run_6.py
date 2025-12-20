77e23e2d496a7d5a35392d7b45a5e03ad567fc60
lisaisapizza <ylisadai@gmail.com>
2025-12-20 17:16:18 -0500

    Add files via upload

--- Run_6.py
+++ Run_6.py
@@ -11,11 +11,16 @@ right_side = Motor(Port.E, Direction.CLOCKWISE)
 left_side = Motor(Port.F, Direction.COUNTERCLOCKWISE)
 drive_base = DriveBase(left, right, 62.4, 110)
 
-
+#the initial position of the robot is that its horizontal
+#its touching the vertical thick line and the 2nd or 3rd thin line after the first thick line
+#do the weely thing
+drive_base.straight(-800)
+left_side.run_angle(1000,-2000)
 #Drop off the stuff
-drive_base.straight(-100)
-drive_base.turn(115)
-drive_base.settings(1000)
-drive_base.straight(-900)
-right_side.run_angle(100,50)
+# drive_base.straight(-100)
+# drive_base.turn(115)
+# drive_base.settings(1000)
+# drive_base.straight(-1000)
+# right_side.run_angle(100,50)
+
 
