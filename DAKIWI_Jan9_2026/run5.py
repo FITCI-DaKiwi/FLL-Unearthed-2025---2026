127908548006d452bd4cd166c1b3dfc9471c1437
Angela Xie <jaguarheart0418@gmail.com>
2026-01-10 19:14:48 -0500

    Add files via upload

---
+++ DAKIWI_Jan9_2026/run5.py
@@ -0,0 +1,36 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+left = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+right = Motor(Port.B, Direction.CLOCKWISE)
+right_side = Motor(Port.E, Direction.CLOCKWISE)
+left_side = Motor(Port.F, Direction.COUNTERCLOCKWISE)
+drive_base = DriveBase(left, right, 62.4, 110)
+
+#the initial position of the robot is that its horizontal
+#its touching the vertical second thick line
+#do the wheely thing
+drive_base.use_gyro(True)
+drive_base.straight(-710)
+drive_base.turn(-30)
+drive_base.straight(-70)
+drive_base.turn(40)
+drive_base.straight(-100)
+right_side.run_angle(700,2000)
+#do the chicken thing
+drive_base.turn(-30)
+drive_base.straight(200)
+drive_base.turn(40)
+drive_base.straight(-400)
+drive_base.turn(10)
+drive_base.straight(-120)
+left_side.run_angle(1000,100)
+
+
+
+
+
