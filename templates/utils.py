e9c26edee5891b5dc90f4a92d8fe3dd63c542aeb
Tao2yang <ruotingyang@gmail.com>
2026-04-25 15:59:16 -0400

    utils.py

--- templates/utils.py
+++ templates/utils.py
@@ -41,4 +41,57 @@ def turn_exact(hub, drive_base, left, right, target_degree):
        # wait(100)
 
 
+def turn_pid(hub, drive_base, left, right, target_degree,tolerance=0.2):
+    hub.imu.reset_heading(0)
+    drive_base.settings(turn_rate=1000)
+    drive_base.turn(target_degree)
+    last_error=0
+    while True:
+            error = target_degree - hub.imu.heading()
+            derivative = error - last_error
+            last_error = error
+            if abs(error) <= tolerance:
+                drive_base.stop()
+                break
+            left.run(25*error+100*derivative)            right.run(-25*error-100*derivative)      
+            wait(20)
+
+def safe_straight(drive_base,distance, max_time_ms=None):
+    
+    # Start a backup timer
+    timer = StopWatch()
+    
+    # Start driving, but do not pause the code
+    drive_base.straight(distance, wait=False)
+    
+    # Keep checking as long as the robot is still driving
+    while not drive_base.done():
+        
+        # 1. Did it get physically blocked?
+        if drive_base.stalled():
+            print("Robot stalled! Skipping to next move.")
+            drive_base.stop()
+            break
+            
+        # 2. Did it take too long? (Optional timeout check)
+        if max_time_ms is not None and timer.time() > max_time_ms:
+            print("Time limit reached! Skipping to next move.")
+            drive_base.stop()
+            break
+            
+        # A tiny wait helps Pybricks process motor limits smoothly
+        wait(10)
+
+def safe_motor_turn(motor, speed, angle, max_time_ms=None):
+    timer = StopWatch()
+    # Start turning the motor
+    motor.run_angle(speed, angle, wait=False)   
+    # Wait while it's turning and isn't physically stuck
+    while not motor.done() and not motor.stalled():
+        # Check if it has taken longer than our maximum time
+        if max_time_ms is not None and timer.time() > max_time_ms:
+            break # Shatter the loop!
+            
+    # Stop the motor safely
+    motor.stop()
 
